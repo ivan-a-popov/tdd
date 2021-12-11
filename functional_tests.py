911f78402c5639f039dfc40627d00ac1e057829b
Ivan Popov <ivan-a-popov@ya.ru>
2021-12-11 16:53:05 +0300

    starting tdd

---
+++ functional_tests.py
@@ -0,0 +1,7 @@
+#!/home/ipopov/dev/PycharmProjects/TDD/bin/python3
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+assert 'Django' in browser.title
