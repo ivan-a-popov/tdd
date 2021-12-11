acb7488e0adb9968678b99db12e5a8229c80e5ed
Ivan Popov <ivan-a-popov@ya.ru>
2021-12-11 18:29:37 +0300

    project renamed

--- tdl/asgi.py
+++ tdl/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projectone project.
+ASGI config for tdl project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tdl.settings')
 
 application = get_asgi_application()
--- tdl/wsgi.py
+++ tdl/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectone project.
+WSGI config for tdl project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tdl.settings')
 
 application = get_wsgi_application()
