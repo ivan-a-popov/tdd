c329c9cd8bac52f32f79d08776a50c64c8637007
Ivan Popov <ivan-a-popov@ya.ru>
2023-01-05 17:19:19 +0300

    FT for login via email

---
+++ functional_tests/test_login.py
@@ -0,0 +1,50 @@
+from django.core import mail
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import re
+
+from .base import FunctionalTest
+
+TEST_EMAIL = 'user@example.com'
+SUBJECT = 'Your login link for TDL (ToDoLists)'
+
+
+class LoginTest(FunctionalTest):
+
+    def test_can_get_email_link_to_log_in(self):
+        # User goes to the awesome superlists site
+        # and notices a "Log in" section in the navbar for the first time
+        # It's telling him to enter his email address, so he does
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.NAME, 'email').send_keys(TEST_EMAIL)
+        self.browser.find_element(By.NAME, 'email').send_keys(Keys.ENTER)
+
+        # A message appears telling him an email has been sent
+        self.wait_for(lambda: self.assertIn(
+            'check your email',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        ))
+
+        # He checks his email and finds a message
+        email = mail.outbox[0]  
+        self.assertIn(TEST_EMAIL, email.to)
+        self.assertEqual(email.subject, SUBJECT)
+
+        # It has a url link in it
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail(f'Could not find url in email body:\n{email.body}')
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)
+
+        # He clicks it
+        self.browser.get(url)
+
+        # He is logged in!
+        self.wait_for(
+            lambda: self.browser.find_element(By.LINK_TEXT, 'Log out')
+        )
+        navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
+        self.assertIn(TEST_EMAIL, navbar.text)
+
